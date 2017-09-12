5898da48249f94f8b9981349dc8abbf29919876c
kavithakaruppiah <30867531+kavithakaruppiah@users.noreply.github.com>
2017-09-12 19:45:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var pool=require('pg').pool;
+var config={
+       user: 'kavikarup',
+       database: 'kavikarup',
+       host: 'db.imad.hasura-app.io',
+       port: '5432',
+       password: process.env.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -97,15 +105,33 @@ var htmlTemplate=`
 return htmlTemplate;
 }
 
+var pool=new pool(config);
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/:articleName',function(req,res){
-    var articleName=req.params.articleName
-    res.send(createTemplate(articles[articleName]));
-});
+app.get('/articles/:articleName',function(req,res){
+    
+    pool.query("select * from test where title='"+req.params.articleName+"'",function(err,result){
+        if (err){
+            res.status(500).send(err.toString());
+        }
+        else
+        {
+            if (result.rows.length === 0){
+                res.status(404).send('Article not found');
+            }
+            else{
+                var articleData=result.rows[0];
+                res.send(createTemplate(articleData));
+            }
+        }
+    });
 
+    //var articleName=req.params.articleName
+   // res.send(createTemplate(articles[articleName]));
+});
 
 
 app.get('/ui/style.css', function (req, res) {
